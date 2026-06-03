71d84fa5a9efce75f729f97c23899e627d1bc265
Sebastian Capellan <scapellan55@gmail.com>
2026-06-03 14:19:03 -0400

    Escape exercise from module 2

---
+++ ch2/Escape.cpp
@@ -0,0 +1,15 @@
+/*
+    This program demonstrates the
+    use of escape characters.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "one\ttwo\tthree\n";
+    cout << "123\b\b45";
+
+    return 0;
+}
