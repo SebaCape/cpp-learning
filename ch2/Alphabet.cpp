24c151e9d1b56b9c187ae1ce278444463dbc6974
Sebastian Capellan <scapellan55@gmail.com>
2026-06-03 13:06:30 -0400

    Alphabet exercise from module 2

---
+++ ch2/Alphabet.cpp
@@ -0,0 +1,12 @@
+//This program displays the alphabet.
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for(char letter = 'A'; letter <= 'Z'; letter++)
+        cout << letter << " ";
+
+    return 0;
+}
