54b30769fb32d2d4881bb39508e2d10ee68af75f
Sebastian Capellan <scapellan55@gmail.com>
2026-06-10 17:18:14 -0400

    Added const pointer parameter exercise from module 7.

---
+++ ch7/Const.cpp
@@ -0,0 +1,30 @@
+/*
+    Program demonstrating a 
+    const pointer parameter.
+*/
+
+#include <iostream>
+using namespace std;
+
+//Declare function signature with const pointer parameter
+int negate(const int *val);
+
+int main()
+{
+    //Variable declaration
+    int result;
+    int v = 10;
+
+    //Try to set result to a negated v (use :: for global scope)
+    result = ::negate(&v);
+
+    //Notice that v itself is not mutated in its output
+    cout << v << " negated is " << result;
+
+    return 0;
+}
+
+int negate(const int *val)
+{
+    return -*val;
+}
